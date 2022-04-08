f2a81a3c8c9726b3071d3043693a5d9a0bee92bd
echoaj <aljoslin13@yahoo.com>
2022-04-07 22:35:09 -0700

    Finished post request logic

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Info
 
 
 # Create your views here.
 def home_view(request):
-    return render(request, "home.html", {})
+    text_result = request.POST.get('user_input', False)
+    Info.text = text_result
+    print(Info.text)
+    return render(request, "home.html", {'text_info' : Info.text})
