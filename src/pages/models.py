4cb1d383f88e4c3fa0f69204bc2537e5985e1f08
echoaj <aljoslin13@yahoo.com>
2022-04-10 14:01:09 -0700

    Officially finished text portion of website.  Added clear, delete, functionality. And Style Changes

--- src/pages/models.py
+++ src/pages/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 # Create your models here.
 class Info(models.Model):
-    text = models.TextField(blank=True, null=True)
+    text = models.TextField(blank=True, null=True, default="")
--- src/pages/views.py
+++ src/pages/views.py
@@ -4,7 +4,19 @@ from .models import Info
 
 # Create your views here.
 def home_view(request):
-    text_result = request.POST.get('user_input', False)
-    Info.text = text_result
-    print(Info.text)
-    return render(request, "home.html", {'text_info' : Info.text})
+    if request.method == "GET":
+        text = Info.objects.last().text
+        print(" --------------------------------------- ")
+        print(text)
+        print(" --------------------------------------- ")
+        # print("*" * 40)
+        # text = text.replace("\n", "<br>")
+        # print(text)
+        # print("*" * 40)
+        return render(request, "home.html", {'text_info': text})
+    if request.method == "POST":
+        text = request.POST.get('user_input', False)
+        db = Info(text=text)
+        db.save()
+        return render(request, "home.html", {'text_info': text})
+
