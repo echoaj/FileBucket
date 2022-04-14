87d5ff773cbbdb689f86d32aefe7d0b333ad1b2d
echoaj <aljoslin13@yahoo.com>
2022-04-13 23:49:02 -0700

    Added file deleting feature to server

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Info
 from django.core.files.storage import FileSystemStorage
+import shutil
 
 
 # Create your views here.
@@ -18,16 +19,21 @@ def home_view(request):
             db.save()
         elif "file-upload-button" in request.POST:
             fss = FileSystemStorage()
+            shutil.rmtree(fss.base_location)
             file = request.FILES['doc']
             fss.save(file.name, file)
             result = str(file.name).split('.')
             extension = result.pop()
             name = ".".join(result)
+            title = file.name
+            limit = 35
+            if len(title) > limit:
+                title = title[0:limit] + "...." + extension
 
             data = {'text_info': text,
                     "file_name": name,
                     "file_extension": extension,
-                    "file_title": file.name,
+                    "file_title": title,
                     "file_size": file.size,
                     "file_url": fss.url(file)}
 
