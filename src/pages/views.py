02237f341a1a05b6dd9c64133b24230e1a7d98ce
echoaj <aljoslin13@yahoo.com>
2022-04-15 00:24:34 -0700

    loosely implemented file share

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,9 +4,51 @@ from django.core.files.storage import FileSystemStorage
 import shutil
 
 
+# limits file name size
+def chop_file(filename, limit):
+    extension = str(filename).split('.').pop()
+    title = filename
+    if len(filename) > limit:
+        title = filename[0:limit]
+        title += "...." + extension
+    return title
+
+
+# returns dictionary of general file information
+def dissect_file(file):
+    filename = file.name
+    result = str(filename).split('.')
+    extension = result.pop()
+    name = ".".join(result)
+    title = chop_file(filename, limit=25)
+    data = {"name": name,
+            "extension": extension,
+            "title": title,
+            "size": file.size}
+    return data
+
+
+
 # Create your views here.
 def home_view(request):
+    # Retrieve last text from database
     text = Info.objects.last().text
+
+    # Retrieve name of file in media
+    fss = FileSystemStorage()
+    tuple = fss.listdir(fss.base_location)
+    flatten_list = sum(tuple, [])
+    title = flatten_list[0]
+    # file = fss.open(filename)
+    size = fss.size(title)
+    result = str(title).split('.')
+    extension = result.pop()
+    name = ".".join(result)
+    file_data = {"name": name,
+                 "extension": extension,
+                 "title": title,
+                 "size": size}
+    file_data.update({"url": fss.url(title)})
 
     if request.method == "POST":
         if "text-clear-button" in request.POST:
@@ -18,25 +60,12 @@ def home_view(request):
             db = Info(text=text)
             db.save()
         elif "file-upload-button" in request.POST:
-            fss = FileSystemStorage()
             shutil.rmtree(fss.base_location)
             file = request.FILES['doc']
             fss.save(file.name, file)
-            result = str(file.name).split('.')
-            extension = result.pop()
-            name = ".".join(result)
-            title = file.name
-            limit = 35
-            if len(title) > limit:
-                title = title[0:limit] + "...." + extension
+            file_data = dissect_file(file)
+            file_data.update({"url": fss.url(file)})
+            print(file_data)
+            return render(request, "home.html", {'text_info': text, "file": file_data})
 
-            data = {'text_info': text,
-                    "file_name": name,
-                    "file_extension": extension,
-                    "file_title": title,
-                    "file_size": file.size,
-                    "file_url": fss.url(file)}
-
-            return render(request, "home.html", data)
-
-    return render(request, "home.html", {'text_info': text})
+    return render(request, "home.html", {'text_info': text, "file": file_data})
