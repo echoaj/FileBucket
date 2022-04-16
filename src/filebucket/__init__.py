df670a116e75ba4e9386d6a62a0fcbebf82274d6
echoaj <aljoslin13@yahoo.com>
2022-04-16 16:22:14 -0700

    added button animation

---
+++ src/filebucket/__init__.py
@@ -0,0 +1,7 @@
+
+# Todo Drag and Drop
+# Todo Color Animation on Download Button and Label
+# Todo Fix extension cut off
+# Todo Mobile Support
+# Todo Add Folder Capabilities
+# Todo Group select folders and files
