cc25779a7192456f16975767d580c7552c6d6df0
echoaj <aljoslin13@yahoo.com>
2022-04-17 23:10:02 -0700

    Cleaned up code and made things more efficient

--- src/pages/views.py
+++ src/pages/views.py
@@ -6,7 +6,7 @@ import shutil
 
 
 # limits file name size
-def chop_file(filename, limit):
+def chop_filename(filename, limit):
     extension = str(filename).split('.').pop()
     title = filename
     if len(filename) > limit:
@@ -16,16 +16,16 @@ def chop_file(filename, limit):
 
 
 # returns dictionary of general file information
-def dissect_file(file):
-    filename = file.name
-    result = str(filename).split('.')
-    extension = result.pop()
-    name = ".".join(result)
-    title = chop_file(filename, limit=25)
+def dissect_file(filename: str) -> dict:
+    title = chop_filename(filename, limit=25)
+    split_name = str(filename).split('.')
+    extension = split_name.pop()
+    size = fss.size(filename)
+    name = ".".join(split_name)
     data = {"name": name,
             "extension": extension,
             "title": title,
-            "size": file.size}
+            "size": size}
     return data
 
 
@@ -35,27 +35,13 @@ def home_view(request):
     text = Info.objects.last().text
 
     # Retrieve name of file in media
-    fss = FileSystemStorage()
-    media_root = fss.base_location
     file_data = {}
 
-    if Path(media_root).exists():
-        tuple = fss.listdir(media_root)
-        flatten_list = sum(tuple, [])
-        print(flatten_list)
-        print(list(Path(media_root).glob('*.*')))
-
-        if len(flatten_list) > 0:
-            title = flatten_list[0]
-            size = fss.size(title)
-            result = str(title).split('.')
-            extension = result.pop()
-            name = ".".join(result)
-            file_data = {"name": name,
-                         "extension": extension,
-                         "title": title,
-                         "size": size}
-            file_data.update({"url": fss.url(title)})
+    if media_folder.exists():
+        folder_items = [p.name for p in media_folder.iterdir()]
+        if folder_items:
+            file_data = dissect_file(folder_items[0])
+            file_data.update({"url": fss.url(folder_items[0])})
 
     if request.method == "POST":
         if "text-clear-button" in request.POST:
@@ -67,13 +53,18 @@ def home_view(request):
             db = Info(text=text)
             db.save()
         elif "file-upload-button" in request.POST:
-            if Path(media_root).exists():
-                shutil.rmtree(fss.base_location)
+            if media_folder.exists():
+                shutil.rmtree(media_path)
             file = request.FILES['doc']
             fss.save(file.name, file)
-            file_data = dissect_file(file)
+            file_data = dissect_file(file.name)
             file_data.update({"url": fss.url(file)})
-            print(file_data)
             return render(request, "home.html", {'text_info': text, "file": file_data})
 
     return render(request, "home.html", {'text_info': text, "file": file_data})
+
+
+fss = FileSystemStorage()
+media_path = fss.base_location
+media_folder = Path(media_path)
+
