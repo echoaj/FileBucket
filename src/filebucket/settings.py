8e6d915a8050caf1e825bd113a83776020b32d22
echoaj <aljoslin13@yahoo.com>
2022-04-20 21:54:00 -0700

    Fixed template path error

--- src/filebucket/settings.py
+++ src/filebucket/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'filebucket.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["C:/Users/aljos/OneDrive/Documents/Python/FileBucket/src/templates/"],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
